680d214484ec5463dda500e9ab0314b04a3cf0f4
Bettyforlilyro <yz72047@gmail.com>
2025-09-28 23:04:37 +0800

    接口更新v1

--- src/novel_crawler/NovelCrawlerFactory.py
+++ src/novel_crawler/NovelCrawlerFactory.py
@@ -1,8 +1,8 @@
 import asyncio
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Any, List
-
+from enum import Enum
+from typing import Any, Union
 import aiohttp
 
 
@@ -20,43 +20,69 @@ class NovelMetadata:
     catalog_url: str    # 小说目录URL
 
 
+class SortStrategy(ABC):
+    @abstractmethod
+    async def sort(self, novels: list[NovelMetadata]) -> list[NovelMetadata]:
+        pass
+
+
 class BaseNovelCrawler(ABC):
     """
     小说爬虫的基类。
     """
     @abstractmethod
-    async def get_novel_metadata_async(self, session: aiohttp.ClientSession, url: str) -> Any:
+    async def get_novel_metadata_async(
+            self,
+            url: str,
+            session: aiohttp.ClientSession = None,
+            semaphore: asyncio.Semaphore = None
+    ) -> NovelMetadata:
         """
         从小说URL获取元数据。
 
         参数:
-            url (str): 小说的URL。
-
+            url (str): 小说详情页URL
+            session : 信号量semaphore控制并发
+            semaphore : 异步HTTP会话对象，用于管理共享连接池和Cookie等
         返回:
-            Any: 小说的元数据，包括作者、发布时间、字数等...
+            NovelMetadata: 小说的详情，包括作者、发布时间、字数等...
         """
         pass
 
     @abstractmethod
-    async def get_novel_chapters_list_async(self, session: aiohttp.ClientSession, url: str) -> list[tuple[str, str]]:
+    async def get_novel_chapters_list_async(
+            self,
+            url: str,
+            session: aiohttp.ClientSession = None,
+            semaphore: asyncio.Semaphore = None
+    ) -> list[tuple[str, str]]:
         """
         获取小说的章节列表。
 
         参数:
-            url (str): 小说的URL。
+            url (str): 小说章节列表（目录列表）的URL
+            session : 信号量semaphore控制并发
+            semaphore : 异步HTTP会话对象，用于管理共享连接池和Cookie等
 
         返回:
-            List[(str, str)]: 所有章节URL的列表。(章节标题, 章节URL链接)
+            list[tuple[str, str]]: 所有章节URL的列表。(章节标题, 章节内容URL链接)
         """
         pass
 
     @abstractmethod
-    async def get_novel_chapter_content_async(self, session: aiohttp.ClientSession, chapter_url: str) -> str:
+    async def get_novel_chapter_content_async(
+            self,
+            chapter_url: str,
+            session: aiohttp.ClientSession = None,
+            semaphore: asyncio.Semaphore = None
+    ) -> str:
         """
         从小说章节中获取具体内容。
 
         参数:
-            chapter_url (str): 小说章节的URL。
+            chapter_url (str): 小说某一章节具体内容的URL。
+            session : 信号量semaphore控制并发
+            semaphore : 异步HTTP会话对象，用于管理共享连接池和Cookie等
 
         返回:
             str: 章节的内容。
@@ -64,42 +90,65 @@ class BaseNovelCrawler(ABC):
         pass
 
     @abstractmethod
-    async def get_novel_list_by_tag_async(self, tag: str, sort_method: Any = None) -> List[Any]:
+    async def get_novel_list_by_tag_async(
+            self,
+            tag: str,
+            sort_method: SortStrategy,
+            top_n: int,
+            session: aiohttp.ClientSession = None,
+            semaphore: asyncio.Semaphore = None
+    ) -> list[tuple[str, str, str]]:
         """
         根据标签获取小说列表（异步）。
 
         参数:
-            tag (str): 小说的标签，例如"xuanhuan"、"dushi"、"yanqing"。
-            sort_method (Any): 结果的排序方法，需要是可直接调用的排序器，例如"update_time"、"click"、"subscribe"。
+            tag (str): 小说的分类，例如"xuanhuan"、"dushi"、"yanqing"。
+            sort_method (SortStrategy): 结果的排序方法，需要是可直接调用的排序器，例如"update_time"、"click"、"subscribe"。
+            top_n (int): 返回多少本小说
+            session : 信号量semaphore控制并发
+            semaphore : 异步HTTP会话对象，用于管理共享连接池和Cookie等
 
         返回:
-            List[Any]: 具有此标签的小说的详细信息。
+            list[tuple[str, str, str]]: 具有此标签的一些小说信息。包括书名/作者/详情页URL链接
         """
         pass
 
     @abstractmethod
-    async def get_novel_list_by_author_async(self, author: str, semaphore: asyncio.Semaphore) -> list[tuple[str, str, str]]:
+    async def get_novel_list_by_author_async(
+            self,
+            author: str,
+            session: aiohttp.ClientSession = None,
+            semaphore: asyncio.Semaphore = None
+    ) -> list[tuple[str, str, str]]:
         """
         根据作者获取小说列表。
 
         参数:
             author (str): 小说的作者。
-            semaphore : 控制并发的信号量
+            session : 信号量semaphore控制并发
+            semaphore : 异步HTTP会话对象，用于管理共享连接池和Cookie等
 
         返回:
-            list[tuple[str, str, str]]: 具有此关键词的小说的粗略信息，包括书名/作者/详情页URL链接
+            list[tuple[str, str, str]]: 该作者所写的小说的粗略信息，包括书名/作者/详情页URL链接
         """
         pass
 
     @abstractmethod
-    async def get_novel_list_by_keyword_async(self, keyword: str, semaphore: asyncio.Semaphore, top_n: int = 5) -> list[tuple[str, str, str]]:
+    async def get_novel_list_by_keyword_async(
+            self,
+            keyword: str,
+            top_n: int,
+            session: aiohttp.ClientSession = None,
+            semaphore: asyncio.Semaphore = None
+    ) -> list[tuple[str, str, str]]:
         """
         根据关键词获取小说列表。
 
         参数:
             keyword (str): 小说的关键词。
-            semaphore : 控制并发的信号量
-            top_n (int): 返回相关的结果数量，默认5
+            top_n (int): 返回相关的结果数量
+            session : 信号量semaphore控制并发
+            semaphore : 异步HTTP会话对象，用于管理共享连接池和Cookie等
 
         返回:
             list[tuple[str, str, str]]: 具有此关键词的小说的粗略信息，包括书名/作者/详情页URL链接
@@ -107,7 +156,34 @@ class BaseNovelCrawler(ABC):
         pass
 
     @abstractmethod
-    async def write_novel_content_to_file(self, url: str, file_path: str, semaphore: asyncio.Semaphore):
+    async def get_novel_list_by_rank_async(
+            self,
+            rank_type: Union[str, Enum],
+            top_n: int,
+            session: aiohttp.ClientSession = None,
+            semaphore: asyncio.Semaphore = None
+    ) -> list[tuple[str, str, str]]:
+        """
+        根据不同的榜单（诸如订阅榜、推荐榜、月票榜等）返回排行前几的小说列表.
+
+        参数:
+            rank_type : 枚举类型或者字符串类型，不同的小说网站可能有各种不同的榜单类型
+            top_n (int): 返回相关的结果数量
+            session : 信号量semaphore控制并发
+            semaphore : 异步HTTP会话对象，用于管理共享连接池和Cookie等
+
+        返回:
+            list[tuple[str, str, str]]: 具有此关键词的小说的粗略信息，包括书名/作者/详情页URL链接
+        """
+        pass
+
+    @abstractmethod
+    async def write_novel_content_to_file(
+            self,
+            url: str,
+            file_path: str,
+            semaphore: asyncio.Semaphore = None
+    ):
         """
         将具体的某一本小说保存到指定的文件夹路径下
 
@@ -151,4 +227,3 @@ class NovelCrawlerFactory:
         if crawler is None:
             raise ValueError(f"未找到站点 '{site_name}' 的爬虫")
         return crawler()
-
